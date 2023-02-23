d7b566a5d434d54292051ddfc07bb451939df3ec
rgu3st <robguest@gmail.com>
2023-02-23 16:50:19 -0600

    Created command line parser and basic class for my netcat replacement.

--- notcat.py
+++ notcat.py
@@ -16,8 +16,58 @@ def execute(cmd: str) -> str:
     return output.decode()
 
 
+class NotCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        pass
+
+
+    def listen(self):
+        pass
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description='RJAG Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''
+            Example:
+                notcat.py -t 192.168.1.108 -p 5555 -l -c  # command shell
+            All commands:
+                -c shell
+                -e execute
+                -l listen
+                -p port
+                -t target
+                -u upload
+        ''')
+    )
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('l', '--listen', action='store_true', help='listen')
+    parser.add_argument('p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('t', '--target', default='192.168.1.203)', help='speicifed ip')  # Why this default?
+    parser.add_argument('u', '--upload', help='upload file')
+
+    args = parser.parse_args()
+
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+        nc = NotCat(args, buffer.encode())
+        nc.run()
 
 
 if __name__ == "__main__":
